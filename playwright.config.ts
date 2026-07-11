60b92b90dddc3f03f856c0b0385b48a9efd1fe28
thanhntm2812 <thanhntm2812@gmail.com>
2026-07-12 02:07:39 +0700

    Exclude locator tests from CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  testIgnore: process.env.CI ? ["tests/locator/locator_xpath.spec.ts"] : [],
+  testIgnore: process.env.CI ? ["tests/locator/**"] : [],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
